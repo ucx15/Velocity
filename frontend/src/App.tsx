50ae9d9f3e16f7cad5720e239604e1c037a7fc05
ucx15 <83822686+ucx15@users.noreply.github.com>
2025-07-19 14:39:24 +0530

    Refactor App.tsx to use pages now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
 // import React from 'react'
 
-import AudioPlayer from "./AudioPlayer"
+import Song from "./pages/Song/Song"
 
+
+// Pages
 
 const App = () => {
 	return (
 		<div>
+			{/* TODO: Make different pages for different UIs and functionalites. */}
 			<h1 style={{ fontSize: '3.6rem' }}>Velocity</h1>
 
-			<AudioPlayer
-				filename="WhiteNoise.mp3"
-				title="White Noise"
-				artist="Lo-Fi Collective"
-				cover="https://example.com/cover.jpg"
-			/>
+
+			<Song/>
 
 		</div>
 	)
