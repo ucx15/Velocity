63caed141c8f177bdb389617c54657ed63439190
ucx15 <83822686+ucx15@users.noreply.github.com>
2025-08-09 00:48:33 +0530

    Feat: Display all songs on homepage

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,16 +1,45 @@
-// import React from 'react'
+import React from 'react'
 
 
+import { useEffect, useState } from "react"
 import styles from "./home.module.css"
 
 
+type SongListItem = {
+	title: string;
+	artist: string;
+	duration : number;
+
+}
+
+
 const Home = () => {
-  return (
-	<div className={styles.Home}>
 
+	const [songsList, setSongsList] = useState<[SongListItem]>();
 
-	</div>
-  )
+	useEffect(() => {
+		fetch("http://localhost:5000/api/songs")
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				setSongsList(data);
+			});
+	}, [])
+
+	return (
+		<div className={styles.Home}>
+
+			{songsList && songsList.map((song, index) => (
+				<div key={index}>
+					<h2>{song.title}</h2>
+					<p >{song.artist}</p>
+					<p >{song.duration}s</p>
+				</div>
+			))}
+
+		</div>
+	)
 }
 
 export default Home
